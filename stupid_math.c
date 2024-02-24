ecac2abe32e8447ae03d0bca96a83ffe3b50982f
Andrea Pichetto <andrepiche28@outlook.it>
2024-02-24 16:09:55 +0100

    Hot fix of merge complex_mean

--- stupid_math.c
+++ stupid_math.c
@@ -6,8 +6,8 @@ float mean(int a, int b){
 
 complex_float_t complex_mean(complex_int_t a, complex_int_t b){
 	complex_float_t res = {0,0};
-	res.re = (a.re + b.re)/2;
-	res.im = (a.im + b.im)/2;
+	res.re = mean(a.re, b.re);
+	res.im = mean(a.im, b.im);
 	return res;
 }
 
